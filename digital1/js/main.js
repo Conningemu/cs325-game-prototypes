42962c8e7a1c18673e98d39a695d6a0a07357b2f
Conningemu <krint@yahoo.com>
2017-02-04 15:03:04 -0500

    This should fix everything
    Finally reset everything to see if I'm back on track

--- digital1/js/main.js
+++ digital1/js/main.js
@@ -14,22 +14,44 @@ window.onload = function() {
     "use strict";
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
+    //800,600
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/DigitalGame_Cowboy.png' );
-		game.load.image( 'logo', 'assets/sky.png');
+        game.load.image( 'logo', 'assets/Cowboy.png' );
+		game.load.image( 'sky', 'assets/sky.png');
+		game.load.image( 'ground', 'assets/ground.png');
+		game.load.image( 'ground2', 'assets/ground2.png');
+		game.load.image( 'bullet', 'assets/bullet.png');
+		game.load.image( 'target', 'assets/target.png');
+		game.load.image( 'targetC', 'assets/TargetCenter.png');
     }
     
     var bouncy;
+	var ground; 
+	
+	var target;
+	var targetC;
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-		//game.add.sprite(game.world.centerX, game.world.centerY, 'sky');
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
+		game.add.sprite(0, 0, 'sky');
+		game.add.sprite(0, 500, 'ground');
+		game.add.sprite(100, 100, 'bullet');
+		
+		ground = game.add.sprite(0, 532, 'ground2');
+		game.physics.enable( ground, Phaser.Physics.ARCADE);
+		ground.body.immovable = true;
+		
+		target = game.add.sprite(0, 0,'target');
+		game.physics.enable(target, Phaser.Physics.ARCADE);
+		target.anchor.setTo( 0.5, 0.5 );
+		targetC = game.add.sprite(0, 0,'targetC'); //Added to get extra precision on the target
+		game.physics.enable(targetC, Phaser.Physics.ARCADE);
+		targetC.anchor.setTo( 0.5, 0.5 );
+		
+        bouncy = game.add.sprite( 200, 500, 'logo' );
         bouncy.anchor.setTo( 0.5, 0.5 );
+
         
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( bouncy, Phaser.Physics.ARCADE );
@@ -49,6 +71,16 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+		
+		game.physics.arcade.moveToPointer(target, 800);
+		game.physics.arcade.moveToPointer(targetC, 800);
+		if (Phaser.Rectangle.contains(targetC.body, game.input.x, game.input.y))
+        {
+            target.body.velocity.setTo(0, 0);
+			targetC.body.velocity.setTo(0, 0);
+        }
+		
+		var hitPlatform = game.physics.arcade.collide(bouncy, ground);
+        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 };
