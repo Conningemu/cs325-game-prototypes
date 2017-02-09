a163b80c4e8155af1d56dd9deebc5813b7e1d10b
Conningemu <krint@yahoo.com>
2017-02-09 01:09:00 -0500

    Completed as much as I could :(
    The core loop is working but its lacking the content that i wanted to
    add.
    At least I know how all of this stuff works now

--- digital1/js/main.js
+++ digital1/js/main.js
@@ -18,16 +18,28 @@ window.onload = function() {
     function preload() {
         // Load an image and call it 'logo'.
         game.load.image( 'logo', 'assets/Cowboy.png' );
+		game.load.spritesheet( 'walk', 'assets/Cowboy_Walk.png',64,64,9 );
+		game.load.spritesheet( 'explode', 'assets/Explode.png',128,128,16);
 		game.load.image( 'sky', 'assets/sky.png');
 		game.load.image( 'ground', 'assets/ground.png');
 		game.load.image( 'ground2', 'assets/ground2.png');
 		game.load.image( 'bullet', 'assets/bullet.png');
 		game.load.image( 'target', 'assets/target.png');
 		game.load.image( 'targetC', 'assets/TargetCenter.png');
+		game.load.image( 'post', 'assets/WoodenPost.png');
+		game.load.image( 'target64', 'assets/Target64.png');
+		
+		game.load.audio( 'boom', 'assets/boom.wav');
+		game.load.audio( 'footsteps', 'assets/pitterpatter.wav');
+		game.load.audio( 'pew', 'assets/pew.wav');
     }
     
     var bouncy;
 	var ground; 
+	
+	var boom;
+	var footsteps;
+	var pew;
 	
 	var target;
 	var targetC;
@@ -35,6 +47,14 @@ window.onload = function() {
 	var bullets;
 	var fireRate = 500;
 	var nextFire = 0;
+	
+	var level = 1;
+	var enemiesRem = 1;
+	
+	var enemies;
+	var explosions;
+	
+	var shootStart = false;
     
     function create() {
 		
@@ -44,10 +64,29 @@ window.onload = function() {
 		game.add.sprite(0, 0, 'sky');
 		game.add.sprite(0, 500, 'ground');
 		
+		//sound
+		boom = game.add.audio('boom');
+		footsteps = game.add.audio('footsteps');
+		pew = game.add.audio('pew'); 
+		
 		//The ground that is physical
 		ground = game.add.sprite(0, 532, 'ground2');
 		game.physics.enable( ground, Phaser.Physics.ARCADE);
 		ground.body.immovable = true;
+		
+		//enemy
+		game.add.sprite(675,468,'post');
+		enemies = game.add.group();
+		enemies.enableBody = true;
+		enemies.physicsBodyType = Phaser.Physics.ARCADE;
+		var enemyT64 = enemies.create(658,436, 'target64');
+		
+		//Explosions
+		explosions = game.add.group();
+		explosions.createMultiple(30, 'explode');
+		explosions.forEach(setupEnemy, this);
+		//var explode = enemyT64.animations.add('explode');
+		//enemies.callAll('animations.add','animations','explode');
 		
 		//The Target Icon 
 		target = game.add.sprite(0, 0,'target');
@@ -66,29 +105,64 @@ window.onload = function() {
 		bullets.setAll('checkWorldBounds', true);
 		bullets.setAll('outOfBoundsKill', true);
 		
-		//this is actually the cowboy that i just repurposed
-        bouncy = game.add.sprite( 200, 500, 'logo' );
+		//Bouncy the Cowboy
+		bouncy = game.add.sprite( -300, 500, 'walk' );
+        //bouncy = game.add.sprite( 200, 500, 'walk' );
         bouncy.anchor.setTo( 0.5, 0.5 );
+		var walk = bouncy.animations.add('walk');
 
         
         // Turn on the arcade physics engine for this sprite.
         game.physics.enable( bouncy, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        bouncy.body.collideWorldBounds = false;
+		bouncy.checkWorldBounds = true;
+		bouncy.events.onOutOfBounds.add(walk1, this);
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "I'm Trying!!?", style );
+        var style = { font: "25px Verdana", fill: "#000000", align: "center" };
+        var text = game.add.text( game.world.centerX, 15, "Bouncy's Shooting Range", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
+	
+	function setupEnemy(base)
+	{
+		base.anchor.x = 0.5;
+		base.anchor.y = 0.5;
+		base.animations.add('explode');
+	}
     
     function update() {
+		game.physics.arcade.overlap(bullets, enemies, collisionHandler, null, this);
+		
 		//Shoots gun
-		if (game.input.activePointer.isDown)
+		if (bouncy.x > 200 && enemiesRem > 0)
+		{
+			bouncy.body.velocity.x = 0;
+			bouncy.animations.stop(null,true);
+			shootStart = true;
+			
+		}
+		
+		if(enemiesRem == 0)
+		{
+			walk1(bouncy);
+			//if(bouncy.outOfBounds == true)
+			if(bouncy.x > 850)
+			{
+				bouncy.kill();
+				bouncy.reset(-100, 500);
+				enemies.callAll('revive');
+				enemiesRem = enemiesRem +1;
+			}
+		}
+		
+		if (game.input.activePointer.isDown && shootStart == true && bouncy.body.velocity.x == 0)
 		{
 			shoot();
 		}
+	
 		
 		//Target Movement towards the mouse for aiming
 		game.physics.arcade.moveToPointer(target, 800);
@@ -100,21 +174,44 @@ window.onload = function() {
         }
 		
 		var hitPlatform = game.physics.arcade.collide(bouncy, ground);
-        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 	
-	function shoot() {
+	function walk1(bouncy)
+	{
+		bouncy.animations.play('walk', 30, true);
+		bouncy.body.velocity.x = 175;
+	}
+	
+	function shoot() 
+	{
 
 		if (game.time.now > nextFire && bullets.countDead() > 0)
 		{
+			pew.play();
+			
 			nextFire = game.time.now + fireRate;
 
 			var bullet = bullets.getFirstDead();
 
 			bullet.reset(bouncy.x - 8, bouncy.y - 8);
 
-			game.physics.arcade.moveToPointer(bullet, 300);
+			game.physics.arcade.moveToPointer(bullet, 1200);
 		}
+
+	}
+	
+	function collisionHandler(bullet, enemy)
+	{
+		bullet.kill();
+		enemy.kill();
+		boom.play();
+		
+		var explosion = explosions.getFirstExists(false);
+		explosion.reset(enemy.body.x+30, enemy.body.y+20);
+		explosion.play('explode', 30, false, true);
+		
+		enemiesRem = enemiesRem - 1;
+		level = level+1;
 
 	}
 };
